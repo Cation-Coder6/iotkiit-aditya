5dac4238b0b43ccbf1c25826f15262066649a239
Sahil Kumar <io.sahilkr@gmail.com>
2021-01-25 20:19:57 +0530

    Event header

--- components/bullets/Tick.jsx
+++ components/bullets/Tick.jsx
@@ -1,7 +1,7 @@
 
 const Tick = () => {
   return (
-    <svg className="w-6 h-6 align-middle" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokewidth={2} strokelinecap="round" strokelinejoin="round">
+    <svg className="w-6 h-6 align-middle" width={24} height={24} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokewidth={2} strokelinecap="round" strokeLinejoin="round">
       <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14" />
       <polyline points="22 4 12 14.01 9 11.01" />
     </svg>
