867016d7260ddf2779a245e361d1af80da2c7b0f
rkrohk <rkakar2000@gmail.com>
2021-01-26 00:58:10 +0530

    Moved buttons to the right and changed highlighting

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { Transition } from "@headlessui/react";
 import { useState } from "react";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
+  const router = useRouter();
+
+  const path = router.pathname;
   return (
     <nav className="bg-gray-800">
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
@@ -48,8 +52,8 @@ const Navbar = () => {
               </svg>
             </button>
           </div>
-          <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex-shrink-0 flex items-center">
+          <div className="flex-1 flex md:grid md:grid-cols-2  items-center  grid-cols-2 justify-center md:justify-items-stretch sm:items-center">
+            <div className="flex-shrink-0 flex items-center sm:self-start ">
               <img
                 className="block lg:hidden h-10 w-auto"
                 src="https://iotkiit.in/images/logo_small.png"
@@ -61,35 +65,45 @@ const Navbar = () => {
                 alt="Workflow"
               />
             </div>
-            <div className="hidden sm:block sm:ml-6">
+            <div className="hidden sm:block sm:ml-6 md:justify-self-end ">
               <div className="flex space-x-4">
                 <a
                   href="/"
-                  className="bg-gray-900 text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className={`${
+                    path == "/" ? "bg-gray-900" : ""
+                  } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
                 >
                   Home
                 </a>
                 <a
                   href="/team"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className={`${
+                    path == "/team" ? "bg-gray-900" : ""
+                  } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
                 >
                   Team
                 </a>
                 <a
                   href="/projects"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className={`${
+                    path == "/projects" ? "bg-gray-900" : ""
+                  } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
                 >
                   Projects
                 </a>
                 <a
                   href="/events"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className={`${
+                    path == "/events" ? "bg-gray-900" : ""
+                  } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
                 >
                   Events
                 </a>
                 <a
                   href="/contact"
-                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  className={`${
+                    path == "/contact" ? "bg-gray-900" : ""
+                  } text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium`}
                 >
                   Contact
                 </a>
@@ -131,7 +145,7 @@ const Navbar = () => {
               className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
             >
               Contact
-                </a>
+            </a>
           </div>
         </div>
       </Transition>
