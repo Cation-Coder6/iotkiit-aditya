b336c7531ecc4d01738ad7625267f961bbcf5e4f
SahilKr24 <io.sahilkr@gmail.com>
2021-02-12 19:23:23 +0530

    glide removed

--- pages/work/index.jsx
+++ pages/work/index.jsx
@@ -7,7 +7,7 @@ import { Blog } from "../../components/workPage/BlogCard";
 const SectionHeader = () => {
   return (
     <section className="text-gray-600 body-font">
-      <div className="container px-5 pt-12 mx-auto flex items-center md:flex-row flex-col">
+      <div className="container px-5 lg:pr-28 pt-12 mx-auto flex items-center md:flex-row flex-col">
         <div className="flex flex-col md:pr-10 md:mb-0 mb-6 pr-0 w-full md:w-auto md:text-left text-center">
           <h2 className="text-xs text-indigo-500 tracking-widest font-medium title-font mb-1">
             ROOF PARTY POLAROID
