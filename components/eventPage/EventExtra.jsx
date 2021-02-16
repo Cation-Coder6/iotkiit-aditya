6ff5de305cf984e7f6bc8ab8b0e3591c7f2e23e4
Sahil Kumar <io.sahilkr@gmail.com>
2021-02-16 15:51:20 +0530

    refactored upcoming and extra events

--- components/eventPage/EventExtra.jsx
+++ components/eventPage/EventExtra.jsx
@@ -1,4 +1,4 @@
-const EventExtra = () => {
+const EventExtra = ({extraevents}) => {
   return (
     <section className="text-gray-600 body-font">
       <div className="container px-5 py-24 mx-auto">
@@ -10,38 +10,18 @@ const EventExtra = () => {
           <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
         </div>
         <div className="flex flex-wrap -m-4">
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/720x400" alt="content" />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Chichen Itza</h2>
-              <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
+
+            {extraevents.map((i)=>(
+              <div className="xl:w-1/4 md:w-1/2 p-4">
+              <div className="bg-gray-100 p-6 rounded-lg">
+                <img className="h-40 rounded w-full object-cover object-center mb-6" src={i.imgUrl} alt="content" />
+                <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{i.type}</h3>
+                <h2 className="text-lg text-gray-900 font-medium title-font mb-4">{i.name}</h2>
+                <p className="leading-relaxed text-base">{i.desc}</p>
+              </div>
             </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/721x401" alt="content" />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Colosseum Roma</h2>
-              <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-            </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/722x402" alt="content" />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">Great Pyramid of Giza</h2>
-              <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-            </div>
-          </div>
-          <div className="xl:w-1/4 md:w-1/2 p-4">
-            <div className="bg-gray-100 p-6 rounded-lg">
-              <img className="h-40 rounded w-full object-cover object-center mb-6" src="https://dummyimage.com/723x403" alt="content" />
-              <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">SUBTITLE</h3>
-              <h2 className="text-lg text-gray-900 font-medium title-font mb-4">San Francisco</h2>
-              <p className="leading-relaxed text-base">Fingerstache flexitarian street art 8-bit waistcoat. Distillery hexagon disrupt edison bulbche.</p>
-            </div>
-          </div>
+            ))}
+          
         </div>
       </div>
     </section>
--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -4,39 +4,55 @@ import Head from "next/head";
 import UpcomingCard from "../../components/eventPage/UpcomingCard";
 import FlagshipEvents from "../../components/eventPage/FlagshipEvents";
 
-
-const Main = {
-  "imgUrl": "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png",
-  "name":"AWS CLOUD Training",
-  "date":"Oct'20 - Dec'20",
-  "desc":"An Online course on “AWS Cloud” with hands-on learning experience in the field of cloud computing.",
-  "point1":"AWS Certified Trainer",
-  "point2":"55 Hour Extensive Training",
-  "point3":"Certification by SCE KIIT",
-  "point4":"Internship Opportunity"
+const Flagship = {
+  Main: {
+    "imgUrl": "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png",
+    "name": "AWS CLOUD Training",
+    "date": "Oct'20 - Dec'20",
+    "desc": "An Online course on “AWS Cloud” with hands-on learning experience in the field of cloud computing.",
+    "point1": "AWS Certified Trainer",
+    "point2": "55 Hour Extensive Training",
+    "point3": "Certification by SCE KIIT",
+    "point4": "Internship Opportunity"
+  },
+  Left: {
+    "imgUrl": "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png",
+    "name": "Left Event",
+    "date": "Oct'20 - Dec'20",
+    "desc": "An Online course on “AWS Cloud” with hands-on learning experience in the field of cloud computing.",
+    "point1": "AWS Certified Trainer",
+    "point2": "55 Hour Extensive Training",
+    "point3": "Certification by SCE KIIT",
+    "point4": "Internship Opportunity"
+  },
+  Right: {
+    "imgUrl": "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png",
+    "name": "Right Event",
+    "date": "Oct'20 - Dec'20",
+    "desc": "An Online course on “AWS Cloud” with hands-on learning experience in the field of cloud computing.",
+    "point1": "AWS Certified Trainer",
+    "point2": "55 Hour Extensive Training",
+    "point3": "Certification by SCE KIIT",
+    "point4": "Internship Opportunity"
+  }
 }
 
-const Left = {
-  "imgUrl": "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png",
-  "name":"Left Event",
-  "date":"Oct'20 - Dec'20",
-  "desc":"An Online course on “AWS Cloud” with hands-on learning experience in the field of cloud computing.",
-  "point1":"AWS Certified Trainer",
-  "point2":"55 Hour Extensive Training",
-  "point3":"Certification by SCE KIIT",
-  "point4":"Internship Opportunity"
-}
+const extraevents = [
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+  { imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", type: "Hackathon", name: "HahaLolLmao", desc: "Lorem ipsum Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table." },
+]
 
-const Right = {
-  "imgUrl": "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png",
-  "name":"Right Event",
-  "date":"Oct'20 - Dec'20",
-  "desc":"An Online course on “AWS Cloud” with hands-on learning experience in the field of cloud computing.",
-  "point1":"AWS Certified Trainer",
-  "point2":"55 Hour Extensive Training",
-  "point3":"Certification by SCE KIIT",
-  "point4":"Internship Opportunity"
-}
+const upcoming = [
+  { title: "Aws Training", desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.", imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", date: "24 Jul", year: "2021", link: "https://amazon.in" },
+  { title: "Aws Training", desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.", imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", date: "24 Jul", year: "2021", link: "https://amazon.in" },
+  { title: "Aws Training", desc: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam.", imgUrl: "https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png", date: "24 Jul", year: "2021", link: "https://amazon.in" }
+]
 
 const Events = () => {
   return (
@@ -45,9 +61,7 @@ const Events = () => {
         <title>Events</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-
       <EventHeader />
-
       <section>
         <div className="container max-w-full mx-auto lg:py-0 md:py-0 pt-48 mt-48 sm:mt-4 px-6 ">
           <h1 className="text-center text-4xl text-black font-medium leading-snug uppercase tracking-wider">
@@ -60,17 +74,13 @@ const Events = () => {
           <div className="h-1 mx-auto bg-indigo-200 w-24 opacity-75 mt-4 rounded"></div>
         </div>
         <div class="container px-5 py-12 mx-auto flex flex-wrap">
-          {[1, 2, 3].map((i) => (
-            <UpcomingCard index={i} title="Aws Training" desc="Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium, totam rem aperiam." imgUrl="https://futurumresearch.com/wp-content/uploads/2020/01/aws-logo.png" date="24 Jul" year="2021" link="https://amazon.in" />
+          {upcoming.map((i,index) => (
+            <UpcomingCard index={index+1} title={i.title} desc={i.desc} imgUrl={i.imgUrl} date={i.date} year={i.year} link={i.link} />
           ))}
         </div>
       </section>
-
-
-      <FlagshipEvents main={Main} left={Left} right={Right} />
-
-      <EventExtra />
+      <FlagshipEvents main={Flagship.Main} left={Flagship.Left} right={Flagship.Right} />
+      <EventExtra extraevents={extraevents} />
     </>
   );
 };
