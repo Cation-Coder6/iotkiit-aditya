42aa3435519639abdd0a745a4bf09d02e878e25c
Sahil Kumar <io.sahilkr@gmail.com>
2021-02-18 02:27:57 +0530

    Added Contact Buttons

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import ContactButtons from "../../components/contactPage/Buttons";
 import ContactUs from "../../components/contactPage/ContactUs";
 import HandWave from "../../components/contactPage/HandWave";
 import MapFooter from "../../components/contactPage/MapsFooter";
@@ -12,6 +13,7 @@ const Contact = () => {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <HandWave />
+      <ContactButtons />
       <Testimonials />
       <ContactUs />
       <MapFooter />
