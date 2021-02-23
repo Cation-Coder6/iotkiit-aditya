fc4b2c707418320d207719173cbd656c1ebd5ffc
Sahil Kumar <io.sahilkr@gmail.com>
2021-02-24 02:21:57 +0530

    deleted medium.js and other unused dependencies

--- pages/api/medium.js
+++
@@ -1,9 +0,0 @@
-import Meed from "meed";
-import fetch from "node-fetch";
-export default async (req, res) => {
-  const feed = new Meed({ fetch });
-
-  const user = await feed.user("iot-lab");
-  console.log(user);
-  res.json(user);
-};
